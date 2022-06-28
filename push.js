b93343ab501b9bb2353527d7abbe8c5da20b9f19
Eden2911 <edenitysukhlain@gmail.com>
2022-06-28 10:49:00 +0530

    fix

--- push.js
+++ push.js
@@ -7,5 +7,13 @@ let vapidKeys = {
 
 push.setVapidDetails('mailto:edenitysukhlain@gmail.com', vapidKeys.publicKey, vapidKeys.privateKey)
 
-let sub = {};
+let sub = {
+    endpoint:"https://fcm.googleapis.com/fcm/send/cDdqydd4HeY:APA91bEAif3L4Q2-vV3Wv_22wL5Wf1HuqwbnEbsdOq5WGfmzeyNwrr0jDqAnopC9AsmLuYt61ITmhWYghhwh4YcGKeHVAbuNROsEKx5ybHQo3FS8SN7PexKjbDrZQcXeCERM554ETzEq",
+expirationTime :null,
+keys:{
+    p256dh:
+    "BFq1e1pJimi2LUw_hdTrGnTXM-AFly_AwUbaMKdgYYuiF719UAAvUx4jKCiQwSG5vUY5dEsC6GJG57YfAA-rRXM",
+    auth:"LyMtg8ozSSyGFoJraT8jBA"
+}};
+
 push.sendNotification(sub, 'test message')
